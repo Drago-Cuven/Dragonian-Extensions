632e4e1cf3ec757d3a07e7e11ed6a4e1f9d00358
Drago Cuven <MartinelplayzYT@gmail.com>
2025-04-09 15:16:57 -0500

    .

--- Dragonian3D-NMA.js
+++ Dragonian3D-NMA.js
@@ -26,7 +26,7 @@
     Camera: ["#FF0000", "#0000ff", "#0000ff"],      // Pure Red
     Operators: ["#59C059", "#0000ff", "#0000ff"],   // Green
     Pen: ["#0FBD8C", "#0000ff", "#0000ff"],         // Teal
-    Lighting: ["#D1B894", "#0000ff", "#0000ff"],    // Gold
+    Lighting: ["#C9A87C", "#0000ff", "#0000ff"],    // Gold
     Hitboxes: ["#6A6865", "#0000ff", "#0000ff"]     // Gravel
 };
 
